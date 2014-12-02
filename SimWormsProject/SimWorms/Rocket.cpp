d09c5fff8518927262c661029e48807c48558985
Nathan <nathan_boisjoli_7@hotmail.com>
2014-12-02 10:39:47 -0500

    commit

---
+++ SimWormsProject/SimWorms/Rocket.cpp
@@ -0,0 +1,36 @@
+#include "Rocket.h"
+#include "ExplosionsManager.h"
+
+Rocket::Rocket(sdl::Vector2Float &startingPosition, sdl::Vector2Float &orientation, float force) {
+	sprite.setTexture(&AssetsManager::getInstance().getTexture("data/textures/doom_rocket.png"));
+	sprite.setPosition(sdl::Vector2Float(startingPosition.x, startingPosition.y));
+	velocity = (orientation * force);
+	sprite.setOrigin(0, (0 + (sprite.getBounds().h / 2)));
+	std::cout << sprite.getPosition().x << sprite.getPosition().y << std::endl;
+	dead = false;
+}
+
+bool Rocket::isDead() {
+	return dead;
+}
+
+void Rocket::update(float frametime, Terrain &terrain) {
+	//physics->update(this, terrain, frametime);
+	if (terrain.isPixelSolid(sprite.getPosition())) {
+		ExplosionsManager::getInstance().addExplosion(sprite.getPosition(), terrain, 75);
+		dead = true;
+	}
+
+	if (sdl::Keyboard::isKeyPressed(SDLK_0))
+		sprite.rotate(2);
+	if (sdl::Keyboard::isKeyPressed(SDLK_9))
+		sprite.rotate(-2);
+	if (sdl::Keyboard::isKeyPressed(SDLK_LEFT))
+		sprite.move(-50 * frametime, 0);
+	if (sdl::Keyboard::isKeyPressed(SDLK_RIGHT))
+		sprite.move(50 * frametime, 0);
+}
+
+void Rocket::draw(sdl::Window &target) {
+	target.draw(&sprite);
+}
