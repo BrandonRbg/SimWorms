ed4cdaf2734aeac38813f4fae1f4d66314f01b54
Nathan Boisjoli <nathan_boisjoli_7@hotmail.com>
2014-12-01 23:46:12 -0500

    Travail sur la classe Grenade

--- SimWormsProject/SimWorms/Rocket.cpp
+++ SimWormsProject/SimWorms/Rocket.cpp
@@ -6,7 +6,6 @@ Rocket::Rocket(sdl::Vector2Float &startingPosition, sdl::Vector2Float &orientati
 	sprite.setPosition(sdl::Vector2Float(startingPosition.x, startingPosition.y));
 	velocity = (orientation * force);
 	sprite.setOrigin(0, (0 + (sprite.getBounds().h / 2)));
-	std::cout << sprite.getPosition().x << sprite.getPosition().y << std::endl;
 	dead = false;
 }
 
@@ -20,15 +19,6 @@ void Rocket::update(float frametime, Terrain &terrain) {
 		ExplosionsManager::getInstance().addExplosion(sprite.getPosition(), terrain, 75);
 		dead = true;
 	}
-
-	if (sdl::Keyboard::isKeyPressed(SDLK_0))
-		sprite.rotate(2);
-	if (sdl::Keyboard::isKeyPressed(SDLK_9))
-		sprite.rotate(-2);
-	if (sdl::Keyboard::isKeyPressed(SDLK_LEFT))
-		sprite.move(-50 * frametime, 0);
-	if (sdl::Keyboard::isKeyPressed(SDLK_RIGHT))
-		sprite.move(50 * frametime, 0);
 }
 
 void Rocket::draw(sdl::Window &target) {
